e9afa5218c0cea16059c718d27f71dce0cfd7351
PhapBui <bvphap.tk@gmail.com>
2023-08-24 19:21:16 +0700

    update react-touter-dom

--- Assignment_03/rjs-asm03/src/pages/Error.js
+++ Assignment_03/rjs-asm03/src/pages/Error.js
@@ -2,13 +2,19 @@ import React from "react";
 import NavBar from "../components/NavBar/NavBar.jsx";
 
 const ErrorPage = () => {
-  <>
-    <NavBar />
-    <main>
-      <h1>An error occurred!</h1>
-      <p>Could not find this page!</p>
-    </main>
-  </>;
+  return (
+    <>
+      <NavBar />
+      <main
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <h1>An error occurred!</h1>
+        <p>Could not find this page!</p>
+      </main>
+    </>
+  );
 };
 
 export default ErrorPage;
